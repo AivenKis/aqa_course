f18efa4c04dd2ed674dcd7e215e5567f9b4bd375
AivenKis <aivenkis92@gmail.com>
2026-05-07 11:00:44 +0700

    07.05.26 list changes: 1. практика числовые типы

--- AQA/Практика/✍️ 1 - Числовые типы (int, float).py
+++ AQA/Практика/✍️ 1 - Числовые типы (int, float).py
@@ -5,8 +5,9 @@ def temperature_converter():
     celsius = float(input("Введите температуру в °C:"))
     fahrenheit = (celsius * 1.8) + 32
     return celsius, fahrenheit
+
 celsius, fahrenheit = temperature_converter()
-print(f"{celsius}°C = {fahrenheit:.1f} °F")
+print(f"{celsius:g}°C = {fahrenheit:g}°F")
 
 
 
