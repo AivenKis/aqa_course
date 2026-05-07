222fa21e7aa1c92eed4e6cf06cce4801e5c36000
AivenKis <aivenkis92@gmail.com>
2026-05-07 14:22:19 +0700

    07.05.26 list changes: 1. практика числовые типы (✍️ 2 - budget_calculation)

--- AQA/Практика/Numeric Types (int, float)/✍️ 1 - temperature_converter.py
+++ AQA/Практика/Numeric Types (int, float)/✍️ 1 - temperature_converter.py
@@ -1,9 +1,17 @@
 def temperature_converter():
-    celsius = float(input("Введите значение в °C:"))
-    fahrenheit = (celsius * 1.8) + 32
-    return celsius, fahrenheit
 
-celsius, fahrenheit = temperature_converter()
+    while True:
+        try:
+            value = float(input("Enter temperature in Celsius:"))
+            return value
+        except ValueError:
+            print("ERROR: Please enter a numeric value")
+
+
+
+celsius = temperature_converter()
+fahrenheit = (celsius * 1.8) + 32
+
 print(f"{celsius:g}°C = {fahrenheit:g}°F")
 
 
