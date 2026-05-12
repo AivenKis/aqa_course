9740d508ba31ba3109b4936ab80d06c49f5f783d
AivenKis <aivenkis92@gmail.com>
2026-05-12 14:52:36 +0700

    12.05.26 list changes: 1. практика: strings

---
+++ AQA/Практика/2 - Strings/✍️ 1 - name_analyzer.py
@@ -0,0 +1,56 @@
+def get_name_input (prompt: str) -> str:
+
+    while True:
+        user_input = input(prompt).strip()
+
+        if not user_input:
+            print('The name cannot be empty. \n')
+            continue
+
+        if len(user_input) < 5 or len(user_input) > 25:
+            print('The name must be between 5 and 25. \n')
+            continue
+
+        if not all(c.isalpha() or c.isspace() or c in "-'" for c in user_input):
+            print('The name must contain only letters and spaces, hyphens (-) and apostrophesS. Special characters and numbers are not allowed.\n')
+            continue
+
+        return user_input
+
+def name_analyzer():
+    user_name = get_name_input('Enter your name and surname:')
+
+
+    reversed_name = user_name[::-1]
+    upper_name = user_name.upper()
+    length = len(user_name)
+
+
+    print("\n" + "=" * 40)
+    print(f"В обратном порядке:   ,  {reversed_name}")
+    print(f"Заглавными:           ,  {upper_name}")
+    print(f"Длина строки:         ,  {length}")
+    print("=" * 40)
+
+name_analyzer()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ AQA/Практика/2 - Strings/✍️ 2 - palindrome.py
@@ -0,0 +1,34 @@
+
+def is_palindrome(text:str)->bool:
+
+    cleaned = ''.join(c.lower() for c in text if c.isalnum())
+
+    if not cleaned:
+        return False
+
+
+    return cleaned == cleaned[::-1]
+
+while True:
+    user_input = input("Enter a string to check: ").strip()
+
+    if not user_input:
+        print('The string cannot be empty. \n')
+        continue
+
+
+    if is_palindrome(user_input):
+        print("Palindrome")
+        break
+
+    else:
+        print("Not a palindrome")
+        break
+
+
+
+
+
+
+
+
