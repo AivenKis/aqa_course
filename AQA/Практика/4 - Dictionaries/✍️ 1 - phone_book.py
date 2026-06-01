02c7a333aad36b4700c1e411966d46c9c6bed960
AivenKis <aivenkis92@gmail.com>
2026-06-01 17:13:22 +0700

    01.06.26 list changes: phone_book (add/del/update contact)

--- AQA/Практика/4 - Dictionaries/✍️ 1 - phone_book.py
+++ AQA/Практика/4 - Dictionaries/✍️ 1 - phone_book.py
@@ -13,9 +13,41 @@ phone_book = {
 
 
 def add_contact(phone_book, name, phone):
+    if name in phone_book:
+        old_phone = phone_book[name]
+        print(f"Контакт '{name}' уже существует. Номер телефона обновлен с {old_phone} на {phone}.")
+    else:
+        print(f"Контакт '{name}' добавлен с номером '{phone}'.")
+
     phone_book[name] = phone
+
     return phone_book
-print(phone_book)
+
+
+
+
+def delete_contact(phone_book, name):
+    if name in phone_book:
+        del phone_book[name]
+        print(f"Контакт '{name}' успешно удален.")
+    else:
+        print(f"Контакт '{name}' не найден.")
+
+    return phone_book
+
+
+
+def update_contact(phone_book, name, new_phone):
+
+    if name in phone_book:
+        old_phone = phone_book[name]
+        phone_book[name] = new_phone
+        print(f"Контакт '{name}' обновлен с номера '{old_phone}' на '{new_phone}'.")
+
+    else:
+        print(f"Контакт '{name}' не найден. Невозможно обновить номер телефона.")
+
+    return phone_book
 
 
 
