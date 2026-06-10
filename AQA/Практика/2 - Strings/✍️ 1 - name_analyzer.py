fd03b3f0650b26ed3f44b239dd8452244d76c664
AivenKis <aivenkis92@gmail.com>
2026-06-10 13:39:52 +0700

    10.06.26                         List changes: Ты разделил ввод имени и фамилии на две функции. Использовал while True для повторного запроса. Создал отдельную функцию analyze_name для обработки и вывода. Пытаешься делать красивый вывод с линиями.
    ______________________________________________________________________

--- AQA/Практика/2 - Strings/✍️ 1 - name_analyzer.py
+++ AQA/Практика/2 - Strings/✍️ 1 - name_analyzer.py
@@ -1,12 +1,64 @@
-# получение данных от пользователя и разделение на имя и фамилию
+#                                       Анализатор имени:
+#       0. Пользователь вводит имя и фамилию
+#       1. Выведите их в обратном порядке
+#       2. Сделайте все буквы заглавными
+#       3. Посчитайте общую длину строки
 
-def data_input(prompt:str):
-    full_name = input("Please enter your name and surname: ")
 
-    first_name = full_name.split()[0]
-    last_name = full_name.split()[1]
 
-    return full_name
+
+
+def get_first_name():
+    while True:
+        first_name = input("Enter your first name: ")
+
+        if not first_name:
+            print("ERROR: The first and last name cannot be empty.\n")
+            continue
+
+        if len(first_name) < 3:
+            print("ERROR: Please enter both first name and last name (at least 3 symbols).\n")
+            continue
+
+        return first_name
+
+
+
+def get_last_name():
+    while True:
+        last_name = input("Enter your last name: ")
+
+        if not last_name:
+            print("ERROR: The last name cannot be empty.\n")
+            continue
+
+        if len(last_name) < 3:
+            print("ERROR: Please enter both last name (at least 3 symbols).\n")
+            continue
+
+        return last_name
+
+
+def analyze_name(first_name, last_name):
+
+    reversed_name = f"{last_name} {first_name}"
+    upper_name = reversed_name.upper()
+    total_length = len(first_name) + len(last_name) +1
+
+    print("\n" + "=" * 40)
+    print(f"В обратном порядке:     {reversed_name}")
+    print(f"Заглавными:             {upper_name}")
+    print(f"Длина строки:           {total_length}")
+    print(f"Длина строки:           {total_length}")
+    print("=" * 40)
+
+
+first_name = get_first_name()
+last_name = get_last_name()
+analyze_name(first_name, last_name)
+
+
+
 
 
 
