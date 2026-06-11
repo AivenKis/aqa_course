2794316a3a330ef4a342de328686651ac74ecb99
AivenKis <aivenkis92@gmail.com>
2026-06-11 14:20:24 +0700

    11.06.26                         List changes: Твоё задание на сейчас (улучшение кода): Возьми свой текущий код и улучши его по следующим пунктам:
    Сделай одну универсальную функцию вместо get_first_name() и get_last_name(). Назови её get_name(prompt: str).
    
    Улучши валидацию:
    
    Запрети ввод только из пробелов
    Запрети ввод цифр и специальных символов (используй .isalpha())
    Минимум 2 символа на имя/фамилию
    Исправь вывод:
    
    Убери дублирование строк
    Сделай красивый и последовательный вывод (или полностью на английском, или полностью на русском)
    Сделай так, чтобы при вводе только пробелов программа выдавала ошибку и просила ввести заново.
    
    ______________________________________________________________________

--- AQA/Практика/2 - Strings/✍️ 1 - name_analyzer.py
+++ AQA/Практика/2 - Strings/✍️ 1 - name_analyzer.py
@@ -1,50 +1,37 @@
-def get_first_name():
+def get_name(prompt: str) -> str:
     while True:
-        first_name = input("Enter your first name: ")
+        name = input(prompt).strip()
 
-        if not first_name:
+        if not name:
             print("ERROR: The first and last name cannot be empty.\n")
             continue
 
-        if len(first_name) < 3:
-            print("ERROR: Please enter both first name and last name (at least 3 symbols).\n")
+        if not  name.replace("-", "").replace("'", "").isalpha():
+            print("ERROR: Name should contain only letters.\n")
             continue
 
-        return first_name
-
-
-
-def get_last_name():
-    while True:
-        last_name = input("Enter your last name: ")
-
-        if not last_name:
-            print("ERROR: The last name cannot be empty.\n")
+        if len(name) < 2:
+            print("ERROR: Name must be at least 2 characters long.\n")
             continue
 
-        if len(last_name) < 3:
-            print("ERROR: Please enter both last name (at least 3 symbols).\n")
-            continue
-
-        return last_name
+        return name
 
 
-def analyze_name(first_name, last_name):
+def analyze_name(first_name: str, last_name:str) -> None:
 
     reversed_name = f"{last_name} {first_name}"
     upper_name = reversed_name.upper()
-    total_length = len(first_name) + len(last_name) +1
+    total_length = len(first_name) + len(last_name) + 1
 
     print("\n" + "=" * 40)
     print(f"В обратном порядке:     {reversed_name}")
     print(f"Заглавными:             {upper_name}")
     print(f"Длина строки:           {total_length}")
-    print(f"Длина строки:           {total_length}")
     print("=" * 40)
 
 
-first_name = get_first_name()
-last_name = get_last_name()
+first_name = get_name("Enter your first name: ")
+last_name = get_name("Enter your last name: ")
 analyze_name(first_name, last_name)
 
 
