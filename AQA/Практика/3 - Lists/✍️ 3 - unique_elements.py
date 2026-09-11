a18ef2f8d2055b8c181563053fbf0e2e53a69030
AivenKis <aivenkis92@gmail.com>
2026-09-11 17:38:25 +0700

    11.00.26            List changes:

--- AQA/Практика/3 - Lists/✍️ 3 - unique_elements.py
+++ AQA/Практика/3 - Lists/✍️ 3 - unique_elements.py
@@ -1,25 +1,45 @@
-def unique_elements(lst: list) -> tuple:
+#                                                                     Unique elements:
+#                                                           Given a list with duplicates: [1, 2, 2, 3, 4, 4, 4, 5].
+#                       Create a new list without duplicates. Combine the lists to produce two lists: the first containing no duplicates,
+#                                                           and the second containing only the duplicates.
+
+
+def unique_elements (numbers: list) -> tuple:
     unique = []
     duplicates = []
 
-    for item in lst:
+    for item in numbers:
         if item not in unique:
             unique.append(item)
+
         else:
             if item not in duplicates:
                 duplicates.append(item)
-    return unique, duplicates
 
-
-def filter_numbers(lst: list) -> tuple:
-    unique, duplicates = unique_elements(lst)
     return unique, duplicates
 
 numbers = [1, 2, 2, 3, 4, 4, 4, 5]
-unique, duplicates = filter_numbers(numbers)
+unique, duplicates = unique_elements(numbers)
+
+print("Unique elements", unique)
+print("Duplicates", duplicates)
 
 
-print("Уникальные элементы:", unique)
-print("Дубликаты:", duplicates)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
