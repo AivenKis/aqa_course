f81c49b069d2dddadcaf8951ecde1741faf47665
AivenKis <aivenkis92@gmail.com>
2026-09-15 12:00:53 +0700

    14.09.26            List changes: Phonebook

--- AQA/Практика/4 - Dictionaries/✍️ 2 - character_count.py
+++ AQA/Практика/4 - Dictionaries/✍️ 2 - character_count.py
@@ -1,30 +1,37 @@
-def count_characters(text):
+#                                                                                       Character counting:
+#                                                                                       The user enters a string.
+#                                       Create a dictionary where the keys are the characters of the string and the values are their occurrence counts.
+
+
+def count_characters(text:str) -> dict:
     char_count = {}
+
     for char in text:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
+        char_count[char] = char_count.get(char, 0) + 1
 
     return char_count
 
 
-def print_character_count(char_count):
-    print(char_count)
-    print()
+def print_character_count(char_count: dict):
+    if not  char_count:
+        print("ERROR: The string cannot be empty." )
+        return
+
+    print('\n Character count result:')
+    print('-' * 50)
 
     for char, count in char_count.items():
-        if count == 1:
-            print(f"Символ '{char}' встречается {count} раз" )
-        elif count in (2, 3, 4):
-            print(f"Символ '{char}' встречается {count} раза")
+        if char == " ":
+            print(f"Пробел{' ':14}→ {count} раз")
+        elif char == "\n":
+            print(f"Новая строка{' ':8}→ {count} раз")
         else:
-            print(f"Символ '{char}' встречается {count} раз")
+            print(f"'{char}'{' ':15}→ {count} раз")
+    print("-" * 40)
 
 
-user_input = input('Введите пожалуйста строку для анализа: ')
-result = count_characters(user_input)
-print_character_count(result)
+
+
 
 
 
